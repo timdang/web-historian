75c12210246136fec338132406bef0cac280f9f5
Tim Dang <timothy.dang@gmail.com>
2016-02-04 19:25:26 -0800

    End work on chron.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -8,15 +8,17 @@ var cronJob = new CronJob({
   cronTime: '01 * * * *',
   onTick: function() {
     console.log('I am running');
-    // var urlArrList;
-    // fs.readFile(archiveHelpers.paths.list, 'utf8', function(err, data) {
-    //     if (err) {
-    //       console.log('errrrr');
-    //     }
-    //     urlArrList = data.split('\n');
-    //   });
-    // console.log('urlArrList',urlArrList);
-    // archiveHelpers.downloadUrls(urlArrList);
+
+    var urlArrList;
+    fs.readFile(archiveHelpers.paths.list, 'utf8', function(err, data) {
+        if (err) {
+          console.log('errrrr');
+        }
+        urlArrList = data.split('\n');
+      });
+    console.log('urlArrList',urlArrList);
+    archiveHelpers.downloadUrls(urlArrList);
+
       }, start: true, timeZone: 'America/Los_Angeles'
 });
 
